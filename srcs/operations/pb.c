d18d5344419a6aeec26585e828601a3f335a3786
rmouduri-vm-windows <rmouduri@student.42.fr>
2021-04-20 15:15:11 +0200

    [FIX], pa, pb

--- srcs/operations/pb.c
+++ srcs/operations/pb.c
@@ -6,7 +6,7 @@
 /*   By: rmouduri <rmouduri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 13:02:31 by rmouduri          #+#    #+#             */
-/*   Updated: 2021/04/20 12:21:37 by rmouduri         ###   ########.fr       */
+/*   Updated: 2021/04/20 15:14:52 by rmouduri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	pb(t_list **lista, t_list **listb)
 	t_node	*node;
 
 	if (!lista || !*lista || !(*lista)->head || !(*lista)->tail)
+		return ;
+	if ((*lista)->head->nb == (*lista)->tail->nb)
 		return ;
 	node = (*lista)->head;
 	(*lista)->head = (*lista)->head->next;
