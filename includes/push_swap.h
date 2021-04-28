c5ef42a7c7479dcffe6e77ec4e83d4a32f800f14
rmouduri-vm-windows <rmouduri@student.42.fr>
2021-04-28 15:11:58 +0200

    [ADD] fct that gives which direction to get lower nb

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,15 +6,18 @@
 /*   By: rmouduri <rmouduri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 16:19:17 by rmouduri          #+#    #+#             */
-/*   Updated: 2021/04/19 15:25:26 by rmouduri         ###   ########.fr       */
+/*   Updated: 2021/04/28 15:02:03 by rmouduri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "utils.h"
+
 void	sort(t_list **lista, t_list **listb);
 void	fill_or_write(char *steps, unsigned char *s_len, char *add,
 					  unsigned char ad_len);
+int		get_closest_nb(t_list *list);
 
 #endif
---
+++ srcs/push_swap/get_closest_nb.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_closest_nb.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmouduri <rmouduri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/28 14:42:06 by rmouduri          #+#    #+#             */
+/*   Updated: 2021/04/28 15:11:16 by rmouduri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "utils.h"
+
+static int	get_backward(t_list *list, int forward, int base_nb)
+{
+	t_node	tmp;
+	int		backward;
+
+	backward = 0;
+	tmp = *list->tail;
+	while (tmp.prev)
+	{
+		if (base_nb > tmp.nb)
+			break ;
+		++backward;
+		tmp = *tmp.prev;
+	}
+	if (!tmp.prev && base_nb < tmp.nb)
+		backward = 0;
+	else if (!tmp.prev && base_nb > tmp.nb)
+		++backward;
+	return (backward < forward ? -1 : 1);
+}
+
+int	get_closest_nb(t_list *list)
+{
+	t_node	tmp;
+	int		forward;
+	int		base_nb;
+
+	forward = 0;
+	tmp = *list->head;
+	base_nb = tmp.nb;
+	while (tmp.next)
+	{
+		if (base_nb > tmp.nb)
+			break ;
+		++forward;
+		tmp = *tmp.next;
+	}
+	if (!tmp.next && base_nb < tmp.nb)
+		forward = 0;
+	else if (!tmp.next && base_nb > tmp.nb)
+		++forward;
+	return (forward == 0 ? 1 : get_backward(list, forward, base_nb));
+}
