c8fbf6c5222c0a2afaabd71c08846411397ca898
rmouduri-vm-windows <rmouduri@student.42.fr>
2021-04-29 15:09:34 +0200

    [FIX] get_up_down

--- srcs/push_swap/get_up_down.c
+++ srcs/push_swap/get_up_down.c
@@ -6,7 +6,7 @@
 /*   By: rmouduri <rmouduri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 15:04:33 by rmouduri          #+#    #+#             */
-/*   Updated: 2021/04/29 15:05:07 by rmouduri         ###   ########.fr       */
+/*   Updated: 2021/04/29 15:09:09 by rmouduri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,9 @@ int			get_up_down(t_list *list, int ref, int *max, int *min)
 {
 	*max = get_bigger_nb(list, ref);
 	*min = get_lower_nb(list, ref);
-	if (*max == *min)
-		return (*max > ref ? 1 : -1);
+	if (*max < ref)
+		return (-1);
+	else if (*min > ref)
+		return (1);
 	return (0);
 }
