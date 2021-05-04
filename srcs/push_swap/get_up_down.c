adff819ca82132477552f5a6da9165228ed218d1
rmouduri-vm-windows <rmouduri@student.42.fr>
2021-05-04 13:37:13 +0200

    [FIX] get_up_down

--- srcs/push_swap/get_up_down.c
+++ srcs/push_swap/get_up_down.c
@@ -6,61 +6,57 @@
 /*   By: rmouduri <rmouduri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 15:04:33 by rmouduri          #+#    #+#             */
-/*   Updated: 2021/04/29 15:09:09 by rmouduri         ###   ########.fr       */
+/*   Updated: 2021/05/04 13:36:54 by rmouduri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "utils.h"
 
-static int	get_bigger_nb(t_list *list, int nb)
-{
-	t_node	tmp;
-	int		max;
-
-	tmp = *list->head;
-	max = tmp.nb;
-	while (tmp.next)
-	{
-		if (tmp.nb > nb)
-			break ;
-		max = tmp.nb;
-		tmp = *tmp.next;
-	}
-	while (tmp.next)
-	{
-		if (tmp.nb < max && tmp.nb > nb)
-			max = tmp.nb;
-		tmp = *tmp.next;
-	}
-	if (tmp.nb < max && tmp.nb > nb)
-		max = tmp.nb;
-	return (max);	
-}
-
 static int	get_lower_nb(t_list *list, int nb)
 {
-	t_node	tmp;
+	t_node	*tmp;
 	int		min;
 
-	tmp = *list->head;
-	min = tmp.nb;
-	while (tmp.next)
+	tmp = list->head;
+	while (tmp)
 	{
-		if (tmp.nb < nb)
+		min = tmp->nb;
+		//		printf("min = %d\n", min);
+		if (tmp->nb < nb)
 			break ;
-		min = tmp.nb;
-		tmp = *tmp.next;
+		tmp = tmp->next;
 	}
-	while (tmp.next)
+	while (tmp)
 	{
-		if (tmp.nb > min && tmp.nb < nb)
-			min = tmp.nb;
-		tmp = *tmp.next;
+		if (tmp->nb > min && tmp->nb < nb)
+			min = tmp->nb;
+		tmp = tmp->next;
 	}
-	if (tmp.nb > min && tmp.nb < nb)
-		min = tmp.nb;
-	return (min);	
+	return (min);
+}
+
+static int	get_bigger_nb(t_list *list, int nb)
+{
+	t_node	*tmp;
+	int		max;
+
+	tmp = list->head;
+	while (tmp)
+	{
+		min = tmp->nb;
+		//		printf("max = %d\n", max);
+		if (tmp->nb > nb)
+			break ;
+		tmp = tmp->next;
+	}
+	while (tmp)
+	{
+		if (tmp->nb < max && tmp->nb > nb)
+			max = tmp->nb;
+		tmp = tmp->next;
+	}
+	return (max);
 }
 
 int			get_up_down(t_list *list, int ref, int *max, int *min)
