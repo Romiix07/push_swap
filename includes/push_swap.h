6aa20ed8ffe438e862552f203aabfcafc5da1510
rmouduri-vm-windows <rmouduri@student.42.fr>
2021-05-04 13:53:33 +0200

    [FIX] change get_closest_nb, ret steps + init target

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rmouduri <rmouduri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 16:19:17 by rmouduri          #+#    #+#             */
-/*   Updated: 2021/05/03 11:02:23 by rmouduri         ###   ########.fr       */
+/*   Updated: 2021/05/04 13:52:17 by rmouduri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 void	sort(t_list **lista, t_list **listb);
 void	fill_or_write(char *steps, unsigned char *s_len, char *add,
 					unsigned char ad_len);
-int		get_closest_nb(t_list *list, int max);
+int		get_closest_nb(t_list *list, int max, int *target);
 int		get_lowest_operation(t_list *list, int nb, char tmp, int *min_returned);
 int		where_is_lowest_nb(t_list *list);
 int		get_nth_nb(t_list *list, int n);
--- srcs/push_swap/get_closest_nb.c
+++ srcs/push_swap/get_closest_nb.c
@@ -6,53 +6,49 @@
 /*   By: rmouduri <rmouduri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 14:42:06 by rmouduri          #+#    #+#             */
-/*   Updated: 2021/04/28 15:11:16 by rmouduri         ###   ########.fr       */
+/*   Updated: 2021/05/04 13:52:38 by rmouduri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "utils.h"
 
-static int	get_backward(t_list *list, int forward, int base_nb)
+static int	get_backward(t_list *list, int forward, int base_nb, int *target)
 {
-	t_node	tmp;
+	t_node	*tmp;
 	int		backward;
 
-	backward = 0;
-	tmp = *list->tail;
-	while (tmp.prev)
+	backward = 1;
+	tmp = list->tail;
+	while (tmp)
 	{
-		if (base_nb > tmp.nb)
+		if (base_nb > tmp->nb)
 			break ;
 		++backward;
-		tmp = *tmp.prev;
+		tmp = tmp->prev;
 	}
-	if (!tmp.prev && base_nb < tmp.nb)
-		backward = 0;
-	else if (!tmp.prev && base_nb > tmp.nb)
-		++backward;
-	return (backward < forward ? -1 : 1);
+	if (tmp && backward < forward)
+		*target = tmp->nb;
+	return (backward < forward ? -backward : forward);
 }
 
-int	get_closest_nb(t_list *list, int max)
+int	get_closest_nb(t_list *list, int max, int *target)
 {
-	t_node	tmp;
+	t_node	*tmp;
 	int		forward;
 	int		base_nb;
 
 	forward = 0;
-	tmp = *list->head;
+	tmp = list->head;
 	base_nb = max;
-	while (tmp.next)
+	while (tmp)
 	{
-		if (base_nb > tmp.nb)
+		if (base_nb > tmp->nb)
 			break ;
 		++forward;
-		tmp = *tmp.next;
+		tmp = tmp->next;
 	}
-	if (!tmp.next && base_nb < tmp.nb)
-		forward = 0;
-	else if (!tmp.next && base_nb > tmp.nb)
-		++forward;
-	return (forward == 0 ? 1 : get_backward(list, forward, base_nb));
+	if (tmp)
+		*target = tmp->nb;
+	return (forward == 0 ? forward : get_backward(list, forward, base_nb, target));
 }
